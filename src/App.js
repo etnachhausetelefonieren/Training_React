c641e1434a3831ad27c6dba345eb078dfed75516
etnachhausetelefonieren <107401280+etnachhausetelefonieren@users.noreply.github.com>
2023-08-08 15:56:47 +0200

    homepage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [value, setValue] = useState(0);
   return (
+    
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -17,6 +20,9 @@ function App() {
         >
           Learn React
         </a>
+        <p>Zahl = <span>{value}</span></p>
+       <div> <button onClick={() => setValue(value + 1)}>+</button></div>
+       <div> <button onClick={() => setValue(value - 1)}>-</button></div>
       </header>
     </div>
   );
