cc508896f8e91fddd9c82a7b5c27d03044fa8a7e
etnachhausetelefonieren <107401280+etnachhausetelefonieren@users.noreply.github.com>
2023-08-15 15:37:53 +0200

    yay

---
+++ src/Components/App/BottomNavigationImpl.js
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import RestoreIcon from '@mui/icons-material/Restore';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ArchiveIcon from '@mui/icons-material/Archive';
+import Paper from '@mui/material/Paper';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+
+
+export default function FixedBottomNavigation() {
+  const [value, setValue] = React.useState(0);
+  const ref = React.useRef(null);
+ 
+  return (
+    <Box sx={{ pb: 7 }} ref={ref}>
+      <CssBaseline />
+      
+      <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
+        <BottomNavigation
+          showLabels
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
+        >
+          <BottomNavigationAction label="Home" icon={<HomeOutlinedIcon />} />
+          
+          <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+          
+         
+        </BottomNavigation>
+      </Paper>
+    </Box>
+  );
+}
